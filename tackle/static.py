6e373ebdcc8bb410684d57f4dfb503e1936b7717
Sam Briesemeister <sam.briesemeister@gmail.com>
2016-02-16 01:15:56 -0800

    WIP static cache middleware layer

--- tackle/static.py
+++ tackle/static.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
 import os
+import re
 import hashlib
-from wsgi import RequestInfo
+import datetime
+import json
+from wsgi import RequestInfo, sendfile
+from middleware import Middleware
+
 
 def md5(*text):
     val = hashlib.md5(text[0])
@@ -10,28 +15,101 @@ def md5(*text):
         val.update(m)
     return val.hexdigest()
 
-class StaticCache(object):
+
+class StoredResponse(object):
+    iso8601 = '%Y-%m-%dT%H:%M:%S.%f'
+
+    def __init__(self, environ, headers = []):
+        self.udpated_at = None
+        self.headers = headers
+        self.status = None
+        self.content = None
+        self.environ = environ
+
+    def start_response(self, status, headers):
+        self.status = status
+        self.headers = (headers)
+        self.updated_at = datetime.datetime.now()
+
+    def serialize(self, filename, content):
+        filename_headers = '%s.headers.txt' % filename
+        data = {
+            'updated_at': self.updated_at.strftime(self.iso8601),
+            'status': self.status,
+            'headers': self.headers
+        }
+        json.dump(data, open(filename_headers, 'wb'))
+        with open(filename, 'wb') as output:
+            for line in content:
+                output.write(line)
+
+        # prepare for retransmission
+        self.content = sendfile(self.environ, filename)
+
+    def is_expired(self, ttl=3600):
+        now = datetime.datetime.now()
+        return (datetime.timedelta(seconds = ttl) + self.updated_at) < now
+
+    @classmethod
+    def load(cls, filename, environ):
+        filename_headers = '%s.headers.txt' % filename
+        data = json.load(open(filename_headers, 'rb'))
+        ts = data.get('updated_at', None)
+        headers = data.get('headers', [])
+        instance = cls(environ, headers = [tuple(i) for i in headers])
+        instance.status = data.get('status', None)
+        instance.updated_at = datetime.datetime.strptime(ts, cls.iso8601)
+        instance.content = sendfile(environ, filename)
+        return instance
+
+    def respond(self, start_response, send_body):
+        start_response(str(self.status), self.headers)
+        return self.content if send_body else []
+
+
+class StaticCacheMiddleware(Middleware):
+
+    # TODO: pivot to using webob.Response.from_file; integrates
+    #   - HTTP status line
+    #   - HTTP headers
+    #   - HTTP body content
+
+    RE_HEADER_FORMAT = re.compile(r'([A-Za-z0-9_-]+):\s+([^\n]+)')
 
     def __init__(self, cachepath = ('.static', 'cache'), ttl=3600):
 
         if isinstance(cachepath, (list, tuple)):
             cachepath = os.path.join(*cachepath)
 
-        cachepath = os.path.join(os.getcwd(), cachepath)
+        self.pool = os.path.join(os.getcwd(), cachepath)
+        self.ttl_default = ttl
+
+        super(StaticCacheMiddleware, self).__init__(internal = True)
+
+        try:
+            os.makedirs(self.pool)
+        except OSError, e:
+            pass
 
 
-    def attach(self, handler):
-        """Decorator for WSGI-compatible methods"""
+    def wsgi_request(self, environ, start_response, app):
+        info = RequestInfo(environ)
+
+        if info.method not in ('get', 'head'):
+            pass
+
+        ident = md5(info.path_qs)
+        source_file = os.path.join(self.pool, ident)
+
+        if os.path.isfile(source_file):
+            response = StoredResponse.load(source_file, environ)
+        else:
+            response = StoredResponse(environ)
+            result = app(environ, response.start_response)
+            response.serialize(source_file, result)
+
+        return response.respond(start_response, (info.method == 'get'))
 
 
-        def q(environ, start_response):
-            info = RequestInfo(environ)
-            ident = md5(info.path_qs)
-
-            if ident not in self.cachepool:
-                # TODO: generate content through bandler
-                # TODO: store cached copy
-
-            # TODO: respond with cache & headers
 
 
